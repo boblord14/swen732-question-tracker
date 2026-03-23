bd2a8580d134a4c68cbf542f0f8428fd063ce014
Branden McColm <bwm6109@rit.edu>
2026-03-23 19:39:24 -0400

    Added mockito to pom.xml and removed @BeforeEach

--- src/test/java/questionTrackerTest.java
+++ src/test/java/questionTrackerTest.java
@@ -1,33 +1,31 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import user.User;
-
-import java.io.File;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class questionTrackerTest {
 
     questionTracker qt = new questionTracker();
-    private static final File USERS_FILE = new File("src/main/users.json");
-    private final ObjectMapper mapper = new ObjectMapper();
 
-    @BeforeEach
-    void setUp() throws Exception {
+    @Test
+    void testLoginValidCredentials(){
+        // Simple teacher and student accounts
         User user1 = new User();
         user1.setId(1);
         user1.setUsername("teacher1");
         user1.setPassword("password1");
         user1.setIsTeacher(true);
 
-        User user2 = new User();
-        user2.setId(2);
-        user2.setUsername("student2");
-        user2.setPassword("password2");
-        user2.setIsTeacher(false);
+        User[] mockUsers = {user1};
+        try(MockedStatic<questionTracker> mocked = mockStatic(questionTracker.class, CALLS_REAL_METHODS)){
+            mocked.when(questionTracker::getUsers).thenReturn(mockUsers);
 
-        User[] users = {user1,user2};
-
-        mapper.writerWithDefaultPrettyPrinter().writeValue(USERS_FILE, users);
+            User result1 = questionTracker.logIn("teacher1", "password1");
+            assertNotNull(result1);
+            assertEquals("teacher1", result1.getUsername());
+            assertTrue(result1.getIsTeacher());
+        }
     }
 }
