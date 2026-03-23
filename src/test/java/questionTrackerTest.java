52c6d28dab5a26712a5e52b68a8a5f3d615e2ce9
Branden McColm <bwm6109@rit.edu>
2026-03-23 19:21:29 -0400

    Basic test setup and BeforeEach

--- src/test/java/questionTrackerTest.java
+++ src/test/java/questionTrackerTest.java
@@ -1,12 +1,33 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import user.User;
+
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class questionTrackerTest {
 
     questionTracker qt = new questionTracker();
+    private static final File USERS_FILE = new File("src/main/users.json");
+    private final ObjectMapper mapper = new ObjectMapper();
 
-    @org.junit.jupiter.api.Test
-    void genericAddTest() {
-        assertEquals(4, qt.genericAddTest(2, 2));
-        assertNotEquals(4, qt.genericAddTest(2, 3));
+    @BeforeEach
+    void setUp() throws Exception {
+        User user1 = new User();
+        user1.setId(1);
+        user1.setUsername("teacher1");
+        user1.setPassword("password1");
+        user1.setIsTeacher(true);
+
+        User user2 = new User();
+        user2.setId(2);
+        user2.setUsername("student2");
+        user2.setPassword("password2");
+        user2.setIsTeacher(false);
+
+        User[] users = {user1,user2};
+
+        mapper.writerWithDefaultPrettyPrinter().writeValue(USERS_FILE, users);
     }
 }
