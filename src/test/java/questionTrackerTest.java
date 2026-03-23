968a1359821de25226c0abd3bf23dc2f09858d2b
Branden McColm <bwm6109@rit.edu>
2026-03-23 19:49:37 -0400

    Login Tests completed using Mockito

--- src/test/java/questionTrackerTest.java
+++ src/test/java/questionTrackerTest.java
@@ -19,6 +19,7 @@ class questionTrackerTest {
         user1.setIsTeacher(true);
 
         User[] mockUsers = {user1};
+        // Mockito to use real methods
         try(MockedStatic<questionTracker> mocked = mockStatic(questionTracker.class, CALLS_REAL_METHODS)){
             mocked.when(questionTracker::getUsers).thenReturn(mockUsers);
 
@@ -26,6 +27,64 @@ class questionTrackerTest {
             assertNotNull(result1);
             assertEquals("teacher1", result1.getUsername());
             assertTrue(result1.getIsTeacher());
+        }
+    }
+
+    @Test
+    void testLoginInvalidCredentials(){
+        User user2 = new User();
+        user2.setId(2);
+        user2.setUsername("student2");
+        user2.setPassword("password2");
+        user2.setIsTeacher(false);
+        User[] mockUsers = {user2};
+        try(MockedStatic<questionTracker> mocked = mockStatic(questionTracker.class, CALLS_REAL_METHODS)){
+            mocked.when(questionTracker::getUsers).thenReturn(mockUsers);
+
+            User result =  questionTracker.logIn("student2", "wrongpassword2");
+            assertNull(result);
+        }
+    }
+
+    @Test
+    void testUserDoesNotExist(){
+        User user3 = new User();
+        user3.setId(3);
+        user3.setUsername("student3");
+        user3.setPassword("password3");
+        user3.setIsTeacher(false);
+        User[] mockUsers = {user3};
+
+        try(MockedStatic<questionTracker> mocked = mockStatic(questionTracker.class, CALLS_REAL_METHODS)){
+            mocked.when(questionTracker::getUsers).thenReturn(mockUsers);
+
+            User result =  questionTracker.logIn("newuser", "password3");
+            assertNull(result);
+        }
+    }
+
+    @Test
+    void testGetUsers(){
+        User user1 = new User();
+        user1.setId(1);
+        user1.setUsername("teacher1");
+        user1.setPassword("password1");
+        user1.setIsTeacher(true);
+        User user2 = new User();
+        user2.setId(2);
+        user2.setUsername("student2");
+        user2.setPassword("password2");
+        user2.setIsTeacher(false);
+        User[] mockUsers = {user1, user2};
+
+        try(MockedStatic<questionTracker> mocked = mockStatic(questionTracker.class, CALLS_REAL_METHODS)){
+            mocked.when(questionTracker::getUsers).thenReturn(mockUsers);
+
+            User[] result = questionTracker.getUsers();
+            assertNotNull(result);
+            assertEquals(2, result.length);
+            assertEquals(user1, result[0]);
+            assertEquals(user2, result[1]);
         }
     }
 }
